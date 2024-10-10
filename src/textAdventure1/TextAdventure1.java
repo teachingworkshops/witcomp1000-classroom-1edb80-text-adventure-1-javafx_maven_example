74f4c79b15c385ad060b1800e1f0cafd45103d62
coupetsatwit <coupets@wit.edu>
2024-10-10 16:25:40 -0400

    initialized booleans

--- src/textAdventure1/TextAdventure1.java
+++ src/textAdventure1/TextAdventure1.java
@@ -8,12 +8,9 @@ package textAdventure1;
  */
 public class TextAdventure1 {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)
+	{
+		boolean A_Presed,B_Pressed,C_Pressed,D_Pressed;
 	}
 
 }
