3fd1187e585251930f7f51fbeb5d588cd0c69335
stem-bud <coupet.swami@gmail.com>
2024-10-10 16:49:13 -0400

    ask 2nd question

--- src/textAdventure1/TextAdventure1.java
+++ src/textAdventure1/TextAdventure1.java
@@ -18,6 +18,9 @@ public class TextAdventure1 {
 		System.out.printf("You're trapped in a room by non other than your arch-nemesis!%n");
 		//System.out.printf("Y");
 		name = scanner.next();
+		
+		System.out.println("What would you like to do?");
+		
 	}
 
 }
